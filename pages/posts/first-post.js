1268a4930062d16e6b27df876ac9a4147d6b023d
Adrian Antanon Orozco <aantanon@brothapps.com>
2021-05-27 12:44:55 +0200

    Assets, metadata, CSS and SCSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,23 @@
+import Head from "next/head";
 import Link from "next/link";
+import Alert from "../../components/alert";
+import Layout from "../../components/layout";
 
 const FirstPost = () => {
   return (
-    <>
-      <h1>Primer post del blog!</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home!</a>
-        </Link>
-      </h2>
-    </>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Alert type="success">
+        <h1>Primer post del blog!</h1>
+        <h2>
+          <Link href="/">
+            <a>Back to home!</a>
+          </Link>
+        </h2>
+      </Alert>
+    </Layout>
   );
 };
 
